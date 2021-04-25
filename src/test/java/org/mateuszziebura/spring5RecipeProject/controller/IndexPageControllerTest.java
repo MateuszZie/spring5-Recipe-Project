70f5a0a6bb58d2c81c89925eaf034bc7d4b1ba2b
Mateusz Ziebura <mateusz_860@op.pl>
2021-04-25 17:07:47 +0200

    add testMockMVC to index page controller

--- src/test/java/org/mateuszziebura/spring5RecipeProject/controller/IndexPageControllerTest.java
+++ src/test/java/org/mateuszziebura/spring5RecipeProject/controller/IndexPageControllerTest.java
@@ -7,15 +7,21 @@ import org.mateuszziebura.spring5RecipeProject.repositories.RecipeRepositories;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 
 class IndexPageControllerTest {
 
@@ -31,6 +37,14 @@ class IndexPageControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         indexPageController= new IndexPageController(repositories);
+    }
+    @Test
+    void testMockMVC() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexPageController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
     @Test
     void getIndexPage(){
